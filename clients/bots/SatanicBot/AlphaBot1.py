c67c6e5d2f3d28a10a6b9c31634be7b259eb0665
KevJabPi <kevinfjabbour@free.fr>
2020-06-23 08:37:41 +0200

    Finished modifying Clara's servo code (can be improved on, but it works)

--- clients/bots/SatanicBot/AlphaBot1.py
+++ clients/bots/SatanicBot/AlphaBot1.py
@@ -7,22 +7,28 @@ import math
 class AlphaBot1(AlphaBot):
 	def __init__(self):
 		super().__init__()
-		self.SERVO1 = 27
-		self.SERVO2 = 22
+		self.SERVO1 = 22
+		self.SERVO2 = 27
 		
 		GPIO.setup(self.SERVO1, GPIO.OUT, initial=GPIO.LOW)
 		GPIO.setup(self.SERVO2, GPIO.OUT, initial=GPIO.LOW)
 		
+		print("Servo1 (pin 22) connection")
 		self.p1 = GPIO.PWM(self.SERVO1, 50)
-		p1.start(0)
+		self.p1.start(0)
+		print("Servo1 connected and running")
 		
+		print("Servo2 (pin 27) connection")
 		self.p2 = GPIO.PWM(self.SERVO2, 50)
-		p2.start(0)
+		self.p2.start(0)
+		print("Servo2 connected and running")
 		
 	def lookAt(self, theta, phi):
 		self.p1.ChangeDutyCycle(2.5 + 10.0 * theta / 180)
+		print("looking at theta = "+str(theta))
 		time.sleep(0.02)
 		self.p2.ChangeDutyCycle(2.5 + 10.0 * phi / 180)
+		print("looking at phi = "+str(phi))
 		time.sleep(0.02)
 
 
