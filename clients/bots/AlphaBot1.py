1e9427190349ed5df2e27ace8a0d088c342bab90
KevJab <kevinfjabbour@free.fr>
2020-06-24 12:06:25 +0200

    Overriding lookAt method in AlphaBot1 (test phase)

--- clients/bots/AlphaBot1.py
+++ clients/bots/AlphaBot1.py
@@ -7,6 +7,23 @@ import math
 class AlphaBot1(AlphaBot):
 	def __init__(self):
 		super().__init__()
+		self.SERVO1 = 27
+		self.SERVO2 = 22
+		
+		GPIO.setup(self.SERVO1, GPIO.OUT, initial=GPIO.LOW)
+		GPIO.setup(self.SERVO2, GPIO.OUT, initial=GPIO.LOW)
+		
+		self.p1 = GPIO.PWM(self.SERVO1, 50)
+		p1.start(0)
+		
+		self.p2 = GPIO.PWM(self.SERVO2, 50)
+		p2.start(0)
+		
+	def lookAt(self, theta, phi):
+		self.p1.ChangeDutyCycle(2.5 + 10.0 * theta / 180)
+		time.sleep(0.02)
+		self.p2.ChangeDutyCycle(2.5 + 10.0 * phi / 180)
+		time.sleep(0.02)
 
 
 
