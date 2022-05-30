b810fa3d7ecfda15ef03ac791e9ff928ec17371e
claraRigo <clara.rigaud@sorbonne-universite.fr>
2022-05-30 10:41:10 +0200

    delete old bots

--- clients/bots/Marcello/capim.py
+++
@@ -1,4 +0,0 @@
-from AlphaBot2 import AlphaBot2
-
-bot = Alphabot2()
-bot.capture_image("efe.jpg")
--- clients/bots/SatanicBot/capim.py
+++
@@ -1,7 +0,0 @@
-#from AlphaBot2 import AlphaBot2
-#bot = AlphaBot2()
-
-from AlphaBot1 import AlphaBot1
-bot = AlphaBot1()
-
-bot.capture_image("efe.jpg")
